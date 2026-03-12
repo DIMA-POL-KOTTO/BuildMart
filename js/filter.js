a1b83253a5a8b739c1496382462e4aea4a41ff5a
Dima Polubeiko <dpolubejko@gmail.com>
2026-03-12 11:39:55 +0300

    filter panel elements done

--- js/filter.js
+++ js/filter.js
@@ -37,20 +37,38 @@ function updateSlider(){
     let min = parseInt(minSlider.value)
     let max = parseInt(maxSlider.value)
     if (min > max-10){
-        minSlider.value = min - 10;
-        max = min-10;
+        min = max-10;
+        minSlider.value = min;
     }
     if (max < min+10){
-        maxSlider.value = max + 10;
-        min = max + 10;
+        max = min + 10;
+        maxSlider.value = max;
     }
+    min = Math.max(0, min);
+    max = Math.min(maxPrice, max);
+    minSlider.value = min;
+    maxSlider.value = max;
+
     minValue.textContent = "$" + min;
     maxValue.textContent = "$" + max;
-    const percentMin = (min/maxPrice) * 100;
-    const percentMax = (max/maxPrice) * 100
+    const percentMin = (min/(maxPrice+10)) * 100;
+    const percentMax = (max/(maxPrice+10)) * 100;
     range.style.left = percentMin + "%";
-    
+    range.style.width = (percentMax - percentMin+2.5) + "%";
 }
 minSlider.addEventListener("input", updateSlider)
 maxSlider.addEventListener("input", updateSlider)
 updateSlider();
+
+
+
+const clearBtn = document.getElementById("clearBtn")
+
+clearBtn.addEventListener('click', () => {
+    ratingCheckboxes.forEach(checkbox => {
+        checkbox.checked = false;
+    });
+    minSlider.value = 0;
+    maxSlider.value = 400;
+    updateSlider();
+});
